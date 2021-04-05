c2fd5501b76be88187e8693da7315f3087b8c9fd
Prithvi Nambiar <prithvin@google.com>
2021-04-05 14:04:53 +0530

    Fixes issue related to low impression count.
    If a product had received no impression in the latest data transfer, it was getting excluded from the funnel.
    The product should be added to the funnel if it is approved, in stock, targeted and received any impression in the
    last 30 days.
    
    Change-Id: I413af7faa14fc5d0d9b4d12d10e3a367af554c3d

--- scripts/materialize_product_detailed.sql
+++ scripts/materialize_product_detailed.sql
@@ -75,110 +75,136 @@ BEGIN
           )
         )
       ),
+      ProductMetrics AS (
+        SELECT
+          product_view.latest_date,
+          product_view.unique_product_id,
+          product_metrics_view.externalcustomerid,
+          SUM(product_metrics_view.impressions) AS impressions_30_days,
+          SUM(product_metrics_view.clicks) AS clicks_30_days,
+          SUM(product_metrics_view.cost) AS cost_30_days,
+          SUM(product_metrics_view.conversions) AS conversions_30_days,
+          SUM(product_metrics_view.conversions_value) AS conversions_value_30_days
+        FROM
+          `{project_id}.{dataset}.product_metrics_view` product_metrics_view
+        INNER JOIN
+          `{project_id}.{dataset}.product_view_{merchant_id}` product_view
+          ON
+            product_metrics_view.merchantid = product_view.merchant_id
+            AND LOWER(product_metrics_view.product_id) = LOWER(product_view.product_id)
+            AND product_metrics_view.data_date
+              BETWEEN DATE_SUB(product_view.latest_date, INTERVAL 30 DAY)
+              AND product_view.latest_date
+        WHERE
+          product_view.data_date = product_view.latest_date
+        GROUP BY
+          latest_date,
+          unique_product_id,
+          externalcustomerid
+      ),
       ProductData AS (
-      SELECT
-        product_view.data_date,
-        product_view.latest_date,
-        COALESCE(product_view.aggregator_id, product_view.merchant_id) AS account_id,
-        MAX(customer_view.accountdescriptivename) AS account_display_name,
-        product_view.merchant_id AS sub_account_id,
-        product_view.unique_product_id,
-        MAX(product_view.offer_id) AS offer_id,
-        MAX(product_view.channel) AS channel,
-        MAX(product_view.in_stock) AS in_stock,
-        # An offer is labeled as approved when able to serve on all destinations
-        MAX(CASE
-            WHEN LOWER(destinations.status) <> 'approved' THEN 0
-          ELSE
-          1
-        END
-          ) AS is_approved,
-        # Aggregated Issues & Servability Statuses
-        MAX(disapproval_issues) as disapproval_issues,
-        MAX(demotion_issues) as demotion_issues,
-        MAX(warning_issues) as warning_issues,
-        MIN(IF(TargetedProduct.product_id IS NULL, 0, 1)) AS is_targeted,
-        MAX(title) AS title,
-        MAX(link) AS item_url,
-        MAX(product_type_l1) AS product_type_l1,
-        MAX(product_type_l2) AS product_type_l2,
-        MAX(product_type_l3) AS product_type_l3,
-        MAX(product_type_l4) AS product_type_l4,
-        MAX(product_type_l5) AS product_type_l5,
-        MAX(google_product_category_l1) AS google_product_category_l1,
-        MAX(google_product_category_l2) AS google_product_category_l2,
-        MAX(google_product_category_l3) AS google_product_category_l3,
-        MAX(google_product_category_l4) AS google_product_category_l4,
-        MAX(google_product_category_l5) AS google_product_category_l5,
-        MAX(custom_labels.label_0) AS custom_label_0,
-        MAX(custom_labels.label_1) AS custom_label_1,
-        MAX(custom_labels.label_2) AS custom_label_2,
-        MAX(custom_labels.label_3) AS custom_label_3,
-        MAX(custom_labels.label_4) AS custom_label_4,
-        MAX(product_view.brand) AS brand,
-        MAX(product_metrics_view.impressions_30_days) AS impressions_30_days,
-        MAX(product_metrics_view.clicks_30_days) AS clicks_30_days,
-        MAX(product_metrics_view.cost_30_days) AS cost_30_days,
-        MAX(product_metrics_view.conversions_30_days) AS conversions_30_days,
-        MAX(product_metrics_view.conversions_value_30_days) AS conversions_value_30_days,
-        MAX(description) AS description,
-        MAX(mobile_link) AS mobile_link,
-        MAX(image_link) AS image_link,
-        ANY_VALUE(additional_image_links) AS additional_image_links,
-        MAX(content_language) AS content_language,
-        MAX(target_country) AS target_country,
-        MAX(expiration_date) AS expiration_date,
-        MAX(google_expiration_date) AS google_expiration_date,
-        MAX(adult) AS adult,
-        MAX(age_group) AS age_group,
-        MAX(availability) AS availability,
-        MAX(availability_date) AS availability_date,
-        MAX(color) AS color,
-        MAX(condition) AS condition,
-        MAX(gender) AS gender,
-        MAX(gtin) AS gtin,
-        MAX(item_group_id) AS item_group_id,
-        MAX(material) AS material,
-        MAX(mpn) AS mpn,
-        MAX(pattern) AS pattern,
-        ANY_VALUE(price) AS price,
-        ANY_VALUE(sale_price) AS sale_price,
-        ANY_VALUE(additional_product_types) AS additional_product_types,
-        ANY_VALUE(issues) AS issues
-      FROM
-        `{project_id}.{dataset}.product_view_{merchant_id}` product_view,
-        UNNEST(destinations) AS destinations
-      LEFT JOIN
-        ProductIssuesTable
-        ON
-          ProductIssuesTable.merchant_id = product_view.merchant_id
-          AND ProductIssuesTable.unique_product_id = product_view.unique_product_id
-          AND ProductIssuesTable.data_date = product_view.data_date
-      LEFT JOIN
-        `{project_id}.{dataset}.product_metrics_view` product_metrics_view
-        ON
-          product_metrics_view.merchantid = product_view.merchant_id
-          AND LOWER(product_metrics_view.product_id) = LOWER(product_view.product_id)
-          AND product_metrics_view.data_date = product_metrics_view.latest_date
-      LEFT JOIN
-        `{project_id}.{dataset}.customer_view` customer_view
-        ON
-          customer_view.externalcustomerid = product_metrics_view.externalcustomerid
-          AND customer_view.data_date = customer_view.latest_date
-      LEFT JOIN
-        TargetedProduct
-        ON
-          TargetedProduct.merchant_id = product_view.merchant_id
-          AND TargetedProduct.product_id = product_view.product_id
-      WHERE
-        product_view.data_date = product_view.latest_date
-      GROUP BY
-        data_date,
-        latest_date,
-        account_id,
-        product_view.merchant_id,
-        product_view.unique_product_id
-    )
+        SELECT
+          product_view.data_date,
+          product_view.latest_date,
+          COALESCE(product_view.aggregator_id, product_view.merchant_id) AS account_id,
+          MAX(customer_view.accountdescriptivename) AS account_display_name,
+          product_view.merchant_id AS sub_account_id,
+          product_view.unique_product_id,
+          MAX(product_view.offer_id) AS offer_id,
+          MAX(product_view.channel) AS channel,
+          MAX(product_view.in_stock) AS in_stock,
+          # An offer is labeled as approved when able to serve on all destinations
+          MAX(CASE
+              WHEN LOWER(destinations.status) <> 'approved' THEN 0
+            ELSE
+            1
+          END
+            ) AS is_approved,
+          # Aggregated Issues & Servability Statuses
+          MAX(disapproval_issues) as disapproval_issues,
+          MAX(demotion_issues) as demotion_issues,
+          MAX(warning_issues) as warning_issues,
+          MIN(IF(TargetedProduct.product_id IS NULL, 0, 1)) AS is_targeted,
+          MAX(title) AS title,
+          MAX(link) AS item_url,
+          MAX(product_type_l1) AS product_type_l1,
+          MAX(product_type_l2) AS product_type_l2,
+          MAX(product_type_l3) AS product_type_l3,
+          MAX(product_type_l4) AS product_type_l4,
+          MAX(product_type_l5) AS product_type_l5,
+          MAX(google_product_category_l1) AS google_product_category_l1,
+          MAX(google_product_category_l2) AS google_product_category_l2,
+          MAX(google_product_category_l3) AS google_product_category_l3,
+          MAX(google_product_category_l4) AS google_product_category_l4,
+          MAX(google_product_category_l5) AS google_product_category_l5,
+          MAX(custom_labels.label_0) AS custom_label_0,
+          MAX(custom_labels.label_1) AS custom_label_1,
+          MAX(custom_labels.label_2) AS custom_label_2,
+          MAX(custom_labels.label_3) AS custom_label_3,
+          MAX(custom_labels.label_4) AS custom_label_4,
+          MAX(product_view.brand) AS brand,
+          MAX(ProductMetrics.impressions_30_days) AS impressions_30_days,
+          MAX(ProductMetrics.clicks_30_days) AS clicks_30_days,
+          MAX(ProductMetrics.cost_30_days) AS cost_30_days,
+          MAX(ProductMetrics.conversions_30_days) AS conversions_30_days,
+          MAX(ProductMetrics.conversions_value_30_days) AS conversions_value_30_days,
+          MAX(description) AS description,
+          MAX(mobile_link) AS mobile_link,
+          MAX(image_link) AS image_link,
+          ANY_VALUE(additional_image_links) AS additional_image_links,
+          MAX(content_language) AS content_language,
+          MAX(target_country) AS target_country,
+          MAX(expiration_date) AS expiration_date,
+          MAX(google_expiration_date) AS google_expiration_date,
+          MAX(adult) AS adult,
+          MAX(age_group) AS age_group,
+          MAX(availability) AS availability,
+          MAX(availability_date) AS availability_date,
+          MAX(color) AS color,
+          MAX(condition) AS condition,
+          MAX(gender) AS gender,
+          MAX(gtin) AS gtin,
+          MAX(item_group_id) AS item_group_id,
+          MAX(material) AS material,
+          MAX(mpn) AS mpn,
+          MAX(pattern) AS pattern,
+          ANY_VALUE(price) AS price,
+          ANY_VALUE(sale_price) AS sale_price,
+          ANY_VALUE(additional_product_types) AS additional_product_types,
+          ANY_VALUE(issues) AS issues
+        FROM
+          `{project_id}.{dataset}.product_view_{merchant_id}` product_view,
+          UNNEST(destinations) AS destinations
+        LEFT JOIN
+          ProductIssuesTable
+          ON
+            ProductIssuesTable.merchant_id = product_view.merchant_id
+            AND ProductIssuesTable.unique_product_id = product_view.unique_product_id
+            AND ProductIssuesTable.data_date = product_view.data_date
+        LEFT JOIN
+          ProductMetrics
+          ON
+            ProductMetrics.latest_date = product_view.data_date
+            AND ProductMetrics.unique_product_id = product_view.unique_product_id
+        LEFT JOIN
+          `{project_id}.{dataset}.customer_view` customer_view
+          ON
+            customer_view.externalcustomerid = ProductMetrics.externalcustomerid
+            AND customer_view.data_date = customer_view.latest_date
+        LEFT JOIN
+          TargetedProduct
+          ON
+            TargetedProduct.merchant_id = product_view.merchant_id
+            AND TargetedProduct.product_id = product_view.product_id
+        WHERE
+          product_view.data_date = product_view.latest_date
+        GROUP BY
+          data_date,
+          latest_date,
+          account_id,
+          product_view.merchant_id,
+          product_view.unique_product_id
+      )
     SELECT
       *,
       CASE
