300e1e67732572a76b8e59e8db6ae48cd271e8cd
Prithvi Nambiar <prithvin@google.com>
2021-10-19 22:12:58 +0530

    Avoids target_country usage from Products_* table.
    Change-Id: I3ef3452f5559f96adf34f14a1a585d2d857dcbe9

--- scripts/1_product_view.sql
+++ scripts/1_product_view.sql
@@ -94,77 +94,88 @@ AS (
       MAX(_PARTITIONDATE) AS latest_date
       FROM
       `{project_id}.{dataset}.Products_{merchant_id}`
+    ),
+    ProductStatus AS (
+      SELECT
+        Products._PARTITIONDATE as data_date,
+        LatestDate.latest_date,
+        Products.product_id,
+        Products.merchant_id,
+        Products.aggregator_id,
+        Products.offer_id,
+        Products.title,
+        Products.description,
+        Products.link,
+        Products.mobile_link,
+        Products.image_link,
+        Products.additional_image_links,
+        Products.content_language,
+        COALESCE(
+          ApprovedOffer.target_country,
+          PendingOffer.target_country,
+          DisapprovedOffer.target_country) AS target_country,
+        Products.channel,
+        Products.expiration_date,
+        Products.google_expiration_date,
+        Products.adult,
+        Products.age_group,
+        Products.availability,
+        Products.availability_date,
+        Products.brand,
+        Products.color,
+        Products.condition,
+        Products.custom_labels,
+        Products.gender,
+        Products.gtin,
+        Products.item_group_id,
+        Products.material,
+        Products.mpn,
+        Products.pattern,
+        Products.price,
+        Products.sale_price,
+        Products.google_product_category,
+        Products.google_product_category_path,
+        Products.product_type,
+        Products.additional_product_types,
+        IF(ApprovedOffer.product_id IS NULL, 0, 1) is_approved,
+        CONCAT(CAST(Products.merchant_id AS STRING), '|', Products.product_id)
+          AS unique_product_id,
+        IFNULL(SPLIT(Products.product_type, '>')[SAFE_OFFSET(0)], 'N/A') AS product_type_l1,
+        IFNULL(SPLIT(Products.product_type, '>')[SAFE_OFFSET(1)], 'N/A') AS product_type_l2,
+        IFNULL(SPLIT(Products.product_type, '>')[SAFE_OFFSET(2)], 'N/A') AS product_type_l3,
+        IFNULL(SPLIT(Products.product_type, '>')[SAFE_OFFSET(3)], 'N/A') AS product_type_l4,
+        IFNULL(SPLIT(Products.product_type, '>')[SAFE_OFFSET(4)], 'N/A') AS product_type_l5,
+        IFNULL(SPLIT(Products.google_product_category_path, '>')[SAFE_OFFSET(0)], 'N/A')
+          AS google_product_category_l1,
+        IFNULL(SPLIT(Products.google_product_category_path, '>')[SAFE_OFFSET(1)], 'N/A')
+          AS google_product_category_l2,
+        IFNULL(SPLIT(Products.google_product_category_path, '>')[SAFE_OFFSET(2)], 'N/A')
+          AS google_product_category_l3,
+        IFNULL(SPLIT(Products.google_product_category_path, '>')[SAFE_OFFSET(3)], 'N/A')
+          AS google_product_category_l4,
+        IFNULL(SPLIT(Products.google_product_category_path, '>')[SAFE_OFFSET(4)], 'N/A')
+          AS google_product_category_l5,
+        IF(Products.availability = 'in stock', 1, 0) AS in_stock,
+        IF(MultiChannelTable.product_id IS NULL, 'single_channel', 'multi_channel') AS channel_exclusivity
+      FROM
+        `{project_id}.{dataset}.Products_{merchant_id}` AS Products,
+        LatestDate
+      LEFT JOIN ApprovedOffer USING (_PARTITIONDATE, product_id, merchant_id)
+      LEFT JOIN PendingOffer USING (_PARTITIONDATE, product_id, merchant_id)
+      LEFT JOIN DisapprovedOffer USING (_PARTITIONDATE, product_id, merchant_id)
+      LEFT JOIN MultiChannelTable USING (_PARTITIONDATE, product_id, merchant_id)
     )
   SELECT
-    Products._PARTITIONDATE as data_date,
-    LatestDate.latest_date,
-    Products.product_id,
-    Products.merchant_id,
-    Products.aggregator_id,
-    Products.offer_id,
-    Products.title,
-    Products.description,
-    Products.link,
-    Products.mobile_link,
-    Products.image_link,
-    Products.additional_image_links,
-    Products.content_language,
-    COALESCE(
-      ApprovedOffer.target_country,
-      PendingOffer.target_country,
-      DisapprovedOffer.target_country) AS target_country,
-    Products.channel,
-    Products.expiration_date,
-    Products.google_expiration_date,
-    Products.adult,
-    Products.age_group,
-    Products.availability,
-    Products.availability_date,
-    Products.brand,
-    Products.color,
-    Products.condition,
-    Products.custom_labels,
-    Products.gender,
-    Products.gtin,
-    Products.item_group_id,
-    Products.material,
-    Products.mpn,
-    Products.pattern,
-    Products.price,
-    Products.sale_price,
-    Products.google_product_category,
-    Products.google_product_category_path,
-    Products.product_type,
-    Products.additional_product_types,
-    IF(ApprovedOffer.product_id IS NULL, 0, 1) is_approved,
+    ProductStatus.*,
     OfferIssue.disapproval_issues,
     OfferIssue.demotion_issues,
-    OfferIssue.warning_issues,
-    CONCAT(CAST(Products.merchant_id AS STRING), '|', Products.product_id)
-      AS unique_product_id,
-    IFNULL(SPLIT(Products.product_type, '>')[SAFE_OFFSET(0)], 'N/A') AS product_type_l1,
-    IFNULL(SPLIT(Products.product_type, '>')[SAFE_OFFSET(1)], 'N/A') AS product_type_l2,
-    IFNULL(SPLIT(Products.product_type, '>')[SAFE_OFFSET(2)], 'N/A') AS product_type_l3,
-    IFNULL(SPLIT(Products.product_type, '>')[SAFE_OFFSET(3)], 'N/A') AS product_type_l4,
-    IFNULL(SPLIT(Products.product_type, '>')[SAFE_OFFSET(4)], 'N/A') AS product_type_l5,
-    IFNULL(SPLIT(Products.google_product_category_path, '>')[SAFE_OFFSET(0)], 'N/A')
-      AS google_product_category_l1,
-    IFNULL(SPLIT(Products.google_product_category_path, '>')[SAFE_OFFSET(1)], 'N/A')
-      AS google_product_category_l2,
-    IFNULL(SPLIT(Products.google_product_category_path, '>')[SAFE_OFFSET(2)], 'N/A')
-      AS google_product_category_l3,
-    IFNULL(SPLIT(Products.google_product_category_path, '>')[SAFE_OFFSET(3)], 'N/A')
-      AS google_product_category_l4,
-    IFNULL(SPLIT(Products.google_product_category_path, '>')[SAFE_OFFSET(4)], 'N/A')
-      AS google_product_category_l5,
-    IF(Products.availability = 'in stock', 1, 0) AS in_stock,
-    IF(MultiChannelTable.product_id IS NULL, 'single_channel', 'multi_channel') AS channel_exclusivity
+    OfferIssue.warning_issues
   FROM
-    `{project_id}.{dataset}.Products_{merchant_id}` AS Products,
-    LatestDate
-  LEFT JOIN ApprovedOffer USING (_PARTITIONDATE, product_id, merchant_id, target_country)
-  LEFT JOIN PendingOffer USING (_PARTITIONDATE, product_id, merchant_id, target_country)
-  LEFT JOIN DisapprovedOffer USING (_PARTITIONDATE, product_id, merchant_id, target_country)
-  LEFT JOIN OfferIssue USING (_PARTITIONDATE, product_id, merchant_id, target_country)
-  LEFT JOIN MultiChannelTable USING (_PARTITIONDATE, product_id, merchant_id)
+    ProductStatus
+  LEFT JOIN OfferIssue
+    ON
+      OfferIssue._PARTITIONDATE = ProductStatus.data_date
+      AND OfferIssue.product_id = ProductStatus.product_id
+      AND OfferIssue.merchant_id = ProductStatus.merchant_id
+      AND OfferIssue.target_country = ProductStatus.target_country
 );
